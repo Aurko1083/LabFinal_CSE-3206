26ec3bcb493162e70d1dee327a4138d36ce6107a
Aurko1083 <mahbubaurko23@gmail.com>
2025-04-19 08:24:23 +0600

    Factory Method pattern Base class added.cpp

---
+++ FactroryMethod.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    virtual void drive() = 0;
+    virtual ~Vehicle() = default;
+};
+
+
+
+
+
