7cd01f970a0327d8eacdb737615bf0d3a6039d4c
Aurko1083 <mahbubaurko23@gmail.com>
2025-04-19 08:27:33 +0600

    concrete classes  Implemented

--- FactroryMethod.cpp
+++ FactroryMethod.cpp
@@ -9,7 +9,38 @@ public:
     virtual ~Vehicle() = default;
 };
 
+// Concrete classes
+class Car : public Vehicle {
+public:
+    void drive() override {
+        cout << "Driving a Car!" << endl;
+    }
+};
 
+class Bike : public Vehicle {
+public:
+    void drive() override {
+        cout << "Riding a Bike!" << endl;
+    }
+};
+
+class Truck : public Vehicle {
+public:
+    void drive() override {
+        cout << "Driving a Truck!" << endl;
+    }
+};
+
+// Factory class
+class VehicleFactory {
+public:
+    static unique_ptr<Vehicle> createVehicle(const string& type) {
+        if (type == "Car") return make_unique<Car>();
+        if (type == "Bike") return make_unique<Bike>();
+        if (type == "Truck") return make_unique<Truck>();
+        return nullptr;
+    }
+};
 
 
 
